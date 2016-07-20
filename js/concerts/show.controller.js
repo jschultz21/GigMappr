f5b6863cd6dd2f69e5526a0c5ff723332628cee8
Julia Najafi <Julia.Najafi@gmail.com>
2016-07-20 18:00:02 -0400

    added minor formatting things

--- js/concerts/show.controller.js
+++ js/concerts/show.controller.js
@@ -17,7 +17,7 @@
         ConcertFactory.get({id: $stateParams.id}).$promise.then(function(response){
           vm.concert = response;
 
-          vm.map = { center: { latitude: vm.concert.venue_latitude, longitude: vm.concert.venue_longitude }, zoom: 6, bounds: {} };
+          vm.map = { center: { latitude: vm.concert.venue_latitude, longitude: vm.concert.venue_longitude }, zoom: 12, bounds: {} };
           vm.marker = {
             id: 0,
             coords: {
